017be8c40ecd6c15885437de30d33320a98b9828
Krzysztof Radecki <radeckikrzysztof@gmail.com>
2020-03-15 08:56:48 -0700

    edges raw ptr to unique

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -29,7 +29,7 @@ void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
 
 void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
 {
-    _childEdges.push_back(edge);
+    _childEdges.push_back(std::make_unique<GraphEdge>(*edge));
 }
 
 //// STUDENT CODE
@@ -52,7 +52,7 @@ GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
     //// STUDENT CODE
     ////
 
-    return _childEdges[index];
+    return _childEdges[index].get();
 
     ////
     //// EOF STUDENT CODE
